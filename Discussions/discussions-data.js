3b73217f6c0457635cf98aa85f511cd59781b691
Sandbox Sync <support@getsandbox.com>
2016-04-05 15:11:59 -0400

    Changes

--- Discussions/discussions-data.js
+++ Discussions/discussions-data.js
@@ -1,20 +1,6 @@
 var consts = require('../Common/consts.js').consts,
     utils = require('../Common/utils.js');
 
-var generate = function generate(orgUnit, userId) {
-    var results = {},
-        userIds = utils.getUsers(orgUnit, userId);
-
-    results[orgUnit] = {};
-    
-    for(var j = 0; j < userIds.length; j++){
-        var user = userIds[j];
-        results[orgUnit][user] = { "0": utils.getRandomGrade().toString() };
-    }
-    
-    return results;
-};
-
 var generate = function generate(orgUnit, roleId, start, end) {
     var results = {},
         startDate = new Date(start).setUTCHours(5,0,0,0),
