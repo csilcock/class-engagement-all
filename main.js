907706f66f1507f4a1c8a8f2410b1f380f4a1bff
Sandbox Sync <support@getsandbox.com>
2016-06-16 10:49:47 -0400

    Changes

--- main.js
+++ main.js
@@ -22,6 +22,15 @@ Sandbox.define('/v1/aggregates/10000/data/{orgUnitId}','GET', function(req, res)
     res.json(result);
 });
 
+// Last Day of Course Access of all users for Course - NEW ROUTE
+Sandbox.define('/v1/aggregates/10000/data/','GET', function(req, res) {
+    var result = generateCourseAccessLastDayData(req.params.orgUnitId);
+
+    res.type('application/json');
+    res.status(200);
+    res.json(result);
+});
+
 // Current Grade of all users for Course - NEW ROUTE
 Sandbox.define('/v1/aggregates/20000/data/{orgUnitId}','GET', function(req, res) {
     var result = generateGradesData(req.params.orgUnitId);
@@ -31,8 +40,26 @@ Sandbox.define('/v1/aggregates/20000/data/{orgUnitId}','GET', function(req, res)
     res.json(result);
 });
 
+// Current Grade of all users for Course - NEW ROUTE
+Sandbox.define('/v1/aggregates/20000/data/','GET', function(req, res) {
+    var result = generateGradesData(req.params.orgUnitId);
+
+    res.type('application/json');
+    res.status(200);
+    res.json(result);
+});
+
 // Predicted Grade of all users for Course - NEW ROUTE
 Sandbox.define('/v1/aggregates/30000/data/{orgUnitId}','GET', function(req, res) {
+    var result = generatePredictedGradesData(req.params.orgUnitId);
+
+    res.type('application/json');
+    res.status(200);
+    res.json(result);
+});
+
+// Predicted Grade of all users for Course - NEW ROUTE
+Sandbox.define('/v1/aggregates/30000/data/','GET', function(req, res) {
     var result = generatePredictedGradesData(req.params.orgUnitId);
 
     res.type('application/json');
@@ -51,8 +78,30 @@ Sandbox.define('/v1/aggregates/40000/data/{orgUnitId}','GET', function(req, res)
     res.json(result);
 });
 
+Sandbox.define('/v1/aggregates/40000/data/','GET', function(req, res) {
+    var dates = utils.parseDates(req.query.startTime, req.query.endTime);
+    
+    var result = generateDiscussionsData(req.params.orgUnitId, dates.startTime, dates.endTime);
+
+    res.type('application/json');
+    res.status(200);
+    res.json(result);
+});
+
+
 // Posts replied to
 Sandbox.define('/v1/aggregates/50000/data/{orgUnitId}','GET', function(req, res) {
+    var dates = utils.parseDates(req.query.startTime, req.query.endTime);
+    
+    var result = generateDiscussionsData(req.params.orgUnitId, dates.startTime, dates.endTime);
+
+    res.type('application/json');
+    res.status(200);
+    res.json(result);
+});
+
+// Posts replied to
+Sandbox.define('/v1/aggregates/50000/data/','GET', function(req, res) {
     var dates = utils.parseDates(req.query.startTime, req.query.endTime);
     
     var result = generateDiscussionsData(req.params.orgUnitId, dates.startTime, dates.endTime);
